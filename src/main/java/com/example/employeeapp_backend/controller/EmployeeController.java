25671b7fe8faba3ab025e679993bbfcba550d776
rahulg3520 <rahul@nestgroup.net>
2022-12-05 15:34:01 +0530

    welcome page

---
+++ src/main/java/com/example/employeeapp_backend/controller/EmployeeController.java
@@ -0,0 +1,16 @@
+package com.example.employeeapp_backend.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+
+    @GetMapping("/")
+    public String WelcomePage()
+    {
+        return "Employee Welcome page";
+    }
+
+}
