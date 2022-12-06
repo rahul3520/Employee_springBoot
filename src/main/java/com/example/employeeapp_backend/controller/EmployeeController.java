94c6a705cebf265d8939fc4c3cf47c76838a264a
rahulg3520 <rahul@nestgroup.net>
2022-12-06 10:34:00 +0530

    model creation

--- src/main/java/com/example/employeeapp_backend/controller/EmployeeController.java
+++ src/main/java/com/example/employeeapp_backend/controller/EmployeeController.java
@@ -1,8 +1,10 @@
 package com.example.employeeapp_backend.controller;
 
 
+import com.example.employeeapp_backend.model.Employee;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,34 +16,42 @@ public class EmployeeController {
         return "Employee Welcome page";
     }
 
-    @PostMapping("/add")
-    public String AddEmployee()
+    @PostMapping(path = "/add",produces = "application/json",consumes = "application/json")
+    public String AddEmployee(@RequestBody Employee e)
     {
+        System.out.println(e.getEmpCode().toString());
+        System.out.println(e.getName().toString());
+        System.out.println(e.getDesignation().toString());
+        System.out.println(e.getSalary());
+        System.out.println(e.getCompanyName().toString());
+        System.out.println(e.getMobileNo().toString());
+        System.out.println(e.getUsername().toString());
+        System.out.println(e.getPassword().toString());
         return "Employee add page";
     }
 
-    @PostMapping("/search")
-    public String SearchEmployee()
-    {
-        return "Employee search page";
-    }
-
-    @PostMapping("/edit")
-    public String EditEmployee()
-    {
-        return "Employee edit page";
-    }
-
-    @GetMapping("/viewall")
-    public String ViewallEmployee()
-    {
-        return "View all Employees page";
-    }
-
-    @PostMapping("/delete")
-    public String DeleteEmployee()
-    {
-        return "Delete Employee page";
-    }
+//    @PostMapping(path ="/search",produces = "application/json",consumes = "application/json")
+//    public String SearchEmployee()
+//    {
+//        return "Employee search page";
+//    }
+//
+//    @PostMapping(path = "/edit",produces = "application/json",consumes = "application/json")
+//    public String EditEmployee()
+//    {
+//        return "Employee edit page";
+//    }
+//
+//    @GetMapping(path = "/viewall",produces = "application/json",consumes = "application/json")
+//    public String ViewallEmployee()
+//    {
+//        return "View all Employees page";
+//    }
+//
+//    @PostMapping(path = "/delete",produces = "application/json",consumes = "application/json")
+//    public String DeleteEmployee()
+//    {
+//        return "Delete Employee page";
+//    }
 
 }
---
+++ src/main/java/com/example/employeeapp_backend/model/Employee.java
@@ -0,0 +1,110 @@
+package com.example.employeeapp_backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    private String empCode;
+    private String name;
+    private String designation;
+    private int salary;
+    private String companyName;
+    private String mobileNo;
+    private String username;
+    private String password;
+
+    public Employee() {
+    }
+
+    public Employee(int id, String empCode, String name, String designation, int salary, String companyName, String mobileNo, String username, String password) {
+        this.id = id;
+        this.empCode = empCode;
+        this.name = name;
+        this.designation = designation;
+        this.salary = salary;
+        this.companyName = companyName;
+        this.mobileNo = mobileNo;
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmpCode() {
+        return empCode;
+    }
+
+    public void setEmpCode(String empCode) {
+        this.empCode = empCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getMobileNo() {
+        return mobileNo;
+    }
+
+    public void setMobileNo(String mobileNo) {
+        this.mobileNo = mobileNo;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
